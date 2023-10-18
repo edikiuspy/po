025126f59c0a8f4eaf9257997ffff926dd45ad67
edikiuspy <edikmuntyanovphone@gmail.com>
2023-10-18 21:31:51 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def main(n):
+    n=round(n)
+    if n<=0:
+        raise Exception("Number can't be lower than 1")
+    result = []
+    a = [True] * (n + 1)
+    a[0], a[1] = False, False
+    for i in range(2, int(n ** 0.5) + 1):
+        if a[i]:
+            for j in range(2 * i, n + 1, i):
+                a[j] = False
+    for i in range(2, n):
+        if a[i]:
+            result.append(i)
+    return result
---
+++ test/test_sito.py
@@ -0,0 +1,28 @@
+from sito.main import main
+import pytest
+@pytest.fixture
+def values() -> list[int]:
+    return [10,0,1,50,-10,10.5]
+def test_exists_main_function(values: list[int]) -> None:
+    main(1)
+def test_returns_correct_result(values:list[int]) -> None:
+    result = main(values[0])
+    assert result==[2, 3, 5, 7]
+def test_min_input(values:list[int]) -> None:
+    with pytest.raises(Exception) as e:
+        main(values[1])
+        assert "Number can't be lower than 1" in str(e.value)
+def test_small_input(values:list[int]) -> None:
+    result = main(values[2])
+    assert result == []
+def test_large_input(values:list[int]) -> None:
+    result = main(values[3])
+    assert result==[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]
+def test_negative_input(values:list[int]) -> None:
+    with pytest.raises(Exception) as e:
+        main(values[4])
+        assert "Number can't be lower than 1" in str(e.value)
+def test_float_input(values:list[int]) -> None:
+    with pytest.raises(Exception) as e:
+        main(values[5])
+        assert "Number can't be lower than 1" in str(e.value)
